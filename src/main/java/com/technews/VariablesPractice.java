319199d9a47ead7662207d2606de1665b2c13ab5
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-02 12:46:13 +0000

    User, Post, Comment Entity models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int data = 6;
+        double data2 = 12.1;
+        double sum = data + data2;
+        System.out.println(sum);
+        String word = "word";
+        String concat = word + data;
+        System.out.println(concat);
+    }
+}
