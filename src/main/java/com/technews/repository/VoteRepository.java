2f9aff3f93aa66e4793a1e975aabaaf2251b5e3f
Felicity Brammer <felicitybrammer@gmail.com>
2022-03-02 14:19:15 +0000

    Vote model and Entity interfaces

---
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.technews.repository;
+
+import com.technews.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+}
